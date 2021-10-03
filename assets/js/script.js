e26356cb9982976e919327cc4698e6397d94c463
Sophia Barrett <sophia@yoursummit.media>
2021-10-03 15:25:39 -0700

    add edit task date functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,7 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger("focus");
 });
 
-// when user clicks out of textarea, save text
+// when user clicks out of textarea, save new text
 $(".list-group").on("blur", "textarea", function() {
   // get the textarea's current value/text
   var text = $(this)
@@ -84,6 +84,57 @@ $(".list-group").on("blur", "textarea", function() {
     .addClass("m-1")
     .text(text);
   $(this).replaceWith(taskP);
+});
+
+// when user clicks due date, allow editing
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// when user clicks out of date input, save new date
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the tasks's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to local storage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
 });
 
 
